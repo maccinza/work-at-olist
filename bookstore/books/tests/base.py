b264104db4a18fc0778d825bfca6aa66f37aacad
Lucas Infante <maccinza@gmail.com>
2020-06-25 20:12:49 +0100

    Improves helper function to select unique values from sequence without an infinite loop

--- bookstore/books/tests/base.py
+++ bookstore/books/tests/base.py
@@ -18,11 +18,20 @@ class BaseBooksTests(TestCase):
 
 
 def get_unique_from_sequence(sequence, quantity):
+    """
+    Best effort helper function to select `quantity` unique elements
+    from `sequence`. If it fails to select the given quantity, returns
+    after 5 retries
+    """
     selected = []
+    max_retries = 5
 
-    while len(selected) < quantity:
+    retries = 0
+    while len(selected) < quantity and retries < max_retries:
         author = random.choice(sequence)
         if author not in selected:
             selected.append(author)
+        else:
+            retries += 1
 
     return selected
